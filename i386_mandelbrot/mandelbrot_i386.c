a088059b3e49e199cb9f7223bd324cccfc125c41
aryal <aryalaadi123@gmail.com>
2024-01-22 00:23:09 +0545

    i386_mandelbrot: fixes

--- i386_mandelbrot/mandelbrot_i386.c
+++ i386_mandelbrot/mandelbrot_i386.c
@@ -1,9 +1,9 @@
 #include <dev/vga.h>
 
 // https://rosettacode.org/wiki/Mandelbrot_set#ASCII
-mandelbrot_i386(k){
+int mandelbrot_i386(int k){
 	float i,j,r,x,y=-16;
-	while(vgaputc('\0~'),y++<15)
+	while(vgaputc('\0'),y++<15)
 		for(x=0;x++<80;vgaputc(" .:-;!/>)|&IH%*#"[k&15]))
 			for(i=k=r=0;j=r*r-i*i-2+x/25,i=2*r*i+y/10,j*j+i*i<11&&k++<111;r=j);
 	vgaprint("                                                                          ");
